6e414f71ca7e44e6fb032805e2c7b483c450a5e1
Ali Zafar <ali.zaffar@gmail.com>
2023-12-03 09:39:07 -0500

    template copied

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ collapsibles.forEach((item) =>
         this.classList.toggle("collapsible--expand");
     })
 );
+
+const navbar = document.querySelector(".navbar");
+
+const navbarButton = document.querySelector(".navbar-btn");
+navbarButton.addEventListener("click", function () {
+    navbar.classList.toggle("nav-open");
+    console.log("Menu Clicked");
+});
